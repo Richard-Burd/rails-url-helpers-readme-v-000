da747ebdf7a8aadaf9e22de79c141c16f05574b6
Richard Burd <richardburdx@yahoo.com>
2018-02-15 23:01:13 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
 # resources :posts, only: [:index, :show]
-  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
