a5e1e5b46351e390a5f5cafbaf03928ab6eb61b6
Richard Burd <richardburdx@yahoo.com>
2018-02-15 23:05:42 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-# resources :posts, only: [:index, :show]
-  resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :posts, only: [:index, :show]
+# OK so basically "index" and "show" are rails keywords...they follow CRUD conventions and
+# these seven keywords below are the ones you can use:   
+# resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+# ...you can always run "rake routes" in the command prompt at see what ones are working
 end
